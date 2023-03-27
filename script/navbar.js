0f1915160f5b22c09279fd2dee1c7782b9a2a12b
Deepak Shreevastava <Dpkshreevastava@gmail.com>
2023-03-27 18:28:38 +0530

    sticky navbar got better

--- script/navbar.js
+++ script/navbar.js
@@ -1,5 +1,7 @@
+const toggleStickyClass = 'is-sticky';
 const dropdownBtn = document.getElementById('services');
 const navbar = document.getElementById('navbar');
+const stickyNavbar = document.getElementsByClassName('navbar-sticky')[0];
 const chevron = document.getElementsByClassName('chevron')[0];
 const dropdownList = document.getElementsByClassName('dropdown-list')[0];
 dropdownBtn.addEventListener('click', () => {
@@ -7,6 +9,15 @@ dropdownBtn.addEventListener('click', () => {
   dropdownList.classList.toggle('navbar-animation');
   dropdownList.classList.toggle('display-block');
   chevron.classList.toggle('rotate180');
+});
+
+window.addEventListener('scroll', () => {
+  const currentScroll = window.pageYOffset;
+  if (currentScroll > 100) {
+    stickyNavbar.classList.add('is-sticky');
+  } else {
+    stickyNavbar.classList.remove(toggleStickyClass);
+  }
 });
 
 window.addEventListener('resize', function () {
@@ -35,6 +46,7 @@ hamburger.addEventListener('click', function () {
   if (!isActive) {
     bars[0].style.transform = 'rotate(45deg)';
     hamburger.style.boxShadow = '0 0 15px 1px rgba(0, 0, 0, 0.2)';
+    hamburger.style.backgroundColor = '#ffffff';
     document.body.style.overflow = 'hidden';
     bars[1].style.opacity = '0';
     bars[2].style.transform = 'rotate(-45deg)';
@@ -43,6 +55,7 @@ hamburger.addEventListener('click', function () {
     bars[0].style.transform = 'rotate(0deg)';
     bars[1].style.opacity = '1';
     hamburger.style.boxShadow = '';
+    hamburger.style.backgroundColor = '';
     bars[2].style.transform = 'rotate(0deg)';
     document.body.style.overflow = 'auto';
     isActive = false;
